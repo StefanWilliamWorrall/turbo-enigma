0cd9a529e8ccb8ddf5674c30eed5d501a5d011aa
stefan William-Worrall <stefan.william.worrall@gmail.com>
2025-10-17 11:34:21 +1300

    ensured port 80 can always be included in cors

--- src/WeatherNow.Api/Program.cs
+++ src/WeatherNow.Api/Program.cs
@@ -20,7 +20,7 @@ var frontendUrl = builder.Configuration["Frontend__Url"] ?? "http://localhost:51
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
-        policy.WithOrigins(frontendUrl)
+        policy.WithOrigins(frontendUrl, "http://localhost:80")
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
